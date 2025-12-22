e6c8316ed1dc9a2081bbd8144dcd19c9087f0e5a
Smileyfish <masal5330@hotmail.com>
2025-12-22 11:03:56 +0100

    feat: refactor item handling and enhance CSV export functionality

--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
-from dataclasses import dataclass
 import datetime
+import operator
+from dataclasses import dataclass
+import io
+import csv
 
 items = []
 
@@ -8,7 +11,26 @@ items = []
 class Item:
     text: str
     date: datetime
+    category: str
+    description: str
     isCompleted: bool = False
+
+
+def get_csv():
+    output = io.StringIO()
+    writer = csv.writer(output)
+    writer.writerow(["Titel", "Datum", "Kategorie", "Beschreibung", "Erledigt?"])
+    for item in items:
+        writer.writerow(
+            [
+                item.text,
+                item.date.strftime("%d.%m.%Y"),
+                item.category,
+                item.description,
+                "x" if item.isCompleted else "o",
+            ]
+        )
+    return output.getvalue()
 
 
 def oneWeekFromToday():
@@ -17,14 +39,20 @@ def oneWeekFromToday():
     return today + oneWeek
 
 
-def add(text, date=None):
-    text = text.replace('b', 'bbb').replace('B', 'Bbb')
-
+def add(text, date=None, category=None, description=None):
+    text = text.replace("b", "bbb").replace("B", "Bbb")
     if date is None:
         date = oneWeekFromToday()
     else:
-        date = datetime.datetime.strptime(date, '%Y-%m-%d')
-    items.append(Item(text, date))
+        date = datetime.datetime.strptime(date, "%Y-%m-%d")
+
+    if category is None:
+        category = "default"
+
+    if description is None:
+        description = ""
+    items.append(Item(text, date, category, description))
+    items.sort(key=lambda x: (x.date, x.category))
 
 
 def get_all():
